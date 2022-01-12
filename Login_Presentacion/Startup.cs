ead4cd7071b126e1895a8ccc0b9d79a7e4b56c5c
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2022-01-12 00:25:29 -0400

    Add project files.

---
+++ Login_Presentacion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Login_Presentacion.Startup))]
+namespace Login_Presentacion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
